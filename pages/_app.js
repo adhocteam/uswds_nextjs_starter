9ddc5ab233c2d0709115bc1dad83155b6636c393
Bill Mill <bill@billmill.org>
2023-02-02 10:16:02 -0500

    load scss instead of css

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import Home from "./index";
 
 // It is important to import the USWDS stylesheet in _app.js because
 // it's global https://nextjs.org/docs/messages/css-global
-import "../assets/stylesheets/styles.css";
+import "../assets/stylesheets/styles.scss";
 
 export default function App() {
   return <Home></Home>;
