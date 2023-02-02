5eb65ccd56c1029529b589e2aa46f63ee8240793
Bill Mill <bill@billmill.org>
2023-02-02 12:05:17 -0500

    create a basic _document

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <Script
+          src="assets/js/uswds-init.min.js"
+          strategy="beforeInteractive"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
