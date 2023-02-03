1618567b5ef17aad3806270b9021332b48616ca4
Bill Mill <bill@billmill.org>
2023-02-03 14:53:59 -0500

    fix asset path

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ module.exports = {
   images: {
     unoptimized: true,
   },
+  reactStrictMode: true,
+  basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
   sassOptions: {
     includePaths: [
       path.join(__dirname, "node_modules", "@uswds", "uswds", "packages"),
